fbf81397adaba0e4112decf0a88041fc16c4965b
ankit <ankitpatel9300@gmail.com>
2020-02-18 22:29:07 -0500

    update

--- cstring_sort.c
+++ cstring_sort.c
@@ -5,68 +5,39 @@
 #define N 30
 int compar(void *pa,void *pb);
 void fileCopy(FILE *fp);
- struct person_struct {
-    char lastName[N];
-    char firstName[N];
-    char cityName[N]; } ;
+ struct person_struct
+   {  char lastName[N];
+      char firstName[N];
+      char cityName[N]; } ;
+
  typedef struct person_struct person_t;
- static person_t dBasePeople[100];
+  person_t dBasePeople[100];
+
 int main()
 {
-
-    //strcpy(dBasePeople->lastName,  "Patel");
-    //strcpy(dBasePeople->lastName,  "Ppppp");
-    //strcpy(dBasePeople->firstName, "Ankit");
-    //strcpy(dBasePeople->cityName,  "Toronto");
     FILE *fp=fopen("myTextFile.txt","r");
 
     fileCopy(fp);
-
-    //printf("%s  %s  %s",dBasePeople->lastName,dBasePeople->firstName,dBasePeople->cityName);
+    printf(" %s  %s %s ", dBasePeople[10].lastName,dBasePeople[10].firstName,dBasePeople[10].cityName);
+    printf(" \n%s %s ", dBasePeople[11].lastName,dBasePeople[11].cityName);
+    printf(" \n%s ", dBasePeople[12].lastName);
 
     return 0;
 }
 
 void fileCopy(FILE *fp)
-{
-	char ch;
-	//static char str[05];
-
-	if(fp != NULL)
+{   size_t ctr=0;
+	char str_1[N],str_2[N],str_3[N];
+	while(fscanf(fp,"%s%s%s",str_1,str_2,str_3) != EOF)
 	{
-		int count=0,i=0;
-		while((ch=fgetc(fp))!=EOF )
-		{
-			if(ch==' '|| ch=='\n')
-			{
-				printf("\n");
-				++count;
-			}
-			else
-			{
-				if(i==0) {
-					static char *ptr= &(dBasePeople->lastName);
-					*(dBasePeople->lastName)=ch ;
-					 ptr++ ;           }
-				else if(i==1) { *(dBasePeople->firstName)=ch ;}
-		else if(i==2) {*(dBasePeople->cityName)=ch ; }
-
-				printf("%c",ch);
-
-			//strcpy(dBasePeople->lastName,str);
-			//strcpy(dBasePeople->firstName,str) ;
-			//strcpy(dBasePeople->cityName,str) ;
-
-
-		}
-		//printf("%s  ",dBasePeople->lastName);   //,dBasePeople->firstName,dBasePeople->cityName)
+		strcpy(dBasePeople[ctr].lastName,str_1);
+		strcpy(dBasePeople[ctr].firstName,str_2);
+		strcpy(dBasePeople[ctr].cityName,str_3);
+		//if(ctr<=3)
+	    ++ctr;
 	}
-		printf("%s",dBasePeople->lastName);
-	}
-	else
-	{
-		fclose(fp);
-	}
+     	fclose(fp);
+
 }
 
 int compar(void *pa,void *pb)
@@ -79,3 +50,4 @@ int compar(void *pa,void *pb)
     return 1;
     return 0;
 }
+
